821fb7ba5f15a93feb2e1d726d449afea586fc27
Faris Chew <farischew@gmail.com>
2024-11-19 11:56:38 +0800

    Init

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,119 @@
-const logotext = "JOHN";
+const logotext = "FARIS";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Faris Chew",
+  description:
+    "I'm full-stack developer based in Singapore - working on projects to bring my ideas to life.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Faris Chew",
+  animated: {
+    first: "I love coding",
+    second: "I'm learning Web-3 Development",
+    third: "I take film photos (occasionally)",
+  },
+  description:
+    "I'm full-stack developer based in Singapore - working on projects to bring my ideas to life.",
+  your_img_url: "/faris_photo.JPG",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "About Myself",
+  aboutme:
+    "My career journey has been unconventional. While my background is rooted in Corporate Communications and Strategic Management, I made the dynamic switch to software development when I was a fresh graduate - enrolling myself in a Web Development bootcamp. Since then, I've worked in individual projects and real life products to hone my skills in React.JS and JavaScript.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Alpha Story Technologies",
+    where: "Singapore",
+    date: "2023",
+  },
+  {
+    jobtitle: "Elliot & Co PR Consultancy",
+    where: "Singapore",
+    date: "2021",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "JavaScript",
+    value: 90,
+  },
+  {
+    name: "TypeScript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 85,
+  },
+  {
+    name: "PostgreSQL",
+    value: 80,
+  },
+  {
+    name: "Ruby / Ruby on Rails",
+    value: 60,
+  },
+  {
+    name: "Figma",
+    value: 75,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "Web Development",
+    description:
+      "Skilled in creating intuitive and visually appealing user interfaces that enhance user experiences. I prioritize functionality and accessibility, leveraging tools like Figma and design principles to translate complex requirements into seamless digital interactions.",
+  },
+  {
+    title: "UI & UX Design",
+    description:
+      "Proficient in building responsive and scalable web applications using technologies such as ReactJS, TypeScript, and Redux. I have experience in developing both front-end and back-end solutions, ensuring high performance and maintainability.",
+  },
+  {
+    title: "CI/CD Infrastructure",
+    description:
+      "Experienced in setting up and maintaining CI/CD pipelines to streamline development workflows and automate deployments. I utilize tools like GitHub Actions and Jenkins to ensure continuous integration and delivery, reducing downtime and improving team efficiency",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "/project-images/soundstate-demo-short.gif",
+    description:
+      "A smart playlist curator that allows you to filter your Spotify library according to your mood, and uses AI to generate a unique image for each playlist, for you to share and discover music that you love.",
+    link: "https://github.com/weepotty/soundstate?tab=readme-ov-file",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "farischew@gmail.com",
+  YOUR_FONE: "+65 9851 1106",
+  description:
+    "I'm always open to hear about how we can work together - feel free to reach out to me here.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_2b532cu",
+  YOUR_TEMPLATE_ID: "template_j1abrap",
+  YOUR_USER_ID: "9OtaC2K-2Co6N7ZPt",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com/farischew",
+  linkedin: "https://www.linkedin.com/in/farischew/",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
